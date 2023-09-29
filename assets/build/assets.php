5875f872b24a96f1993d93d66df5d66f62ff3b9e
Tanzim Hossain <tanzimhossain2@gmail.com>
2023-09-29 11:47:29 +0600

    Add: Set up Webpack 5, ES6 with ESLint, PostCSS with Stylelint, CSSNANO

---
+++ assets/build/assets.php
@@ -0,0 +1,1 @@
+<?php return array('js/editor.js' => array('dependencies' => array('wp-polyfill'), 'version' => 'ab59b09e57c34c278851'));
---
+++ xaquila-features.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:       XAquila Features
+ * Plugin URI:        https://github.com/code-BitLabs/aquila-features
+ * Description:       Adds Gutenberg Blocks for Features
+ * Version:           1.0.0
+ * Requires at least: 5.8
+ * Requires PHP:      7.2
+ * Author:            Tanzim Hossain
+ * Author URI:        https://www.twitter.com/kdp_tanzim
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       xaquila-features
+ * Domain Path:       /languages
+ */
